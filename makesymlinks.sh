62c6a0d890bd6892b3792848bd75cb21c70706f4
Jimeno0 <c.p.jimeno0@gmail.com>
2020-05-13 22:11:47 +0200

    add snippets symlink

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -5,6 +5,7 @@ DIR=~/dotfiles
 
 ln -sfv "${DIR}/vim/vimrc" ~/.vimrc
 ln -sfv "${DIR}/vim/vimrc" ~/.config/nvim/init.vim
+ln -sfv "${DIR}/vim/vim" ~/.config/nvim
 ln -sfv "${DIR}/vim/vim" ~/.vim
 ln -sfv "${DIR}/tmux/tmux.conf" ~/.tmux.conf
 ln -sfv "${DIR}/tmux/tmux" ~/.tmux
@@ -13,5 +14,7 @@ ln -sfv "${DIR}/git/gitconfig" ~/.gitconfig
 ln -sfv "${DIR}/ack/ackrc" ~/.ackrc
 ln -sfv "${DIR}/bash/bashrc" ~/.bashrc
 ln -sfv "${DIR}/git/git-prompt.sh" ~/.git-prompt.sh
-
-
+ln -sfv "${DIR}/vim/vim/UltiSnips/javascript.snippets" ~/.config/coc/ultisnips/javascript.snippets
+ln -sfv "${DIR}/vim/vim/UltiSnips/markdown.snippets" ~/.config/coc/ultisnips/markdown.snippets
+ln -sfv "${DIR}/vim/vim/UltiSnips/mdx.snippets" ~/.config/coc/ultisnips/mdx.snippets
+ln -sfv "${DIR}/vim/vim/UltiSnips/html.snippets" ~/.config/coc/ultisnips/html.snippets
