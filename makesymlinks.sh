38fe17acef5abb9091120bf2244ce2c132552ef3
jimeno0 <jimenocontact@gmail.com>
2023-04-09 19:45:36 +0200

    add nvim lua config and ts coc scripts

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -4,8 +4,10 @@ DIR=~/dotfiles
 # Create symlinks to home
 
 ln -sfv "${DIR}/vim/vimrc" ~/.vimrc
-ln -sfv "${DIR}/vim/vimrc" ~/.config/nvim/init.vim
+ln -sfv "${DIR}/vim/nvimrc" ~/.config/nvim/init.vim
+ln -sfv "${DIR}/vim/nvimrc.lua" ~/.config/nvim/nvimrc.lua
 ln -sfv "${DIR}/vim/vim" ~/.config/nvim
+ln -sfv "${DIR}/vim/coc-settings.json" ~/.config/nvim/coc-config.json
 ln -sfv "${DIR}/vim/vim" ~/.vim
 ln -sfv "${DIR}/tmux/tmux.conf" ~/.tmux.conf
 ln -sfv "${DIR}/tmux/tmux" ~/.tmux
@@ -16,6 +18,7 @@ ln -sfv "${DIR}/bash/bashrc" ~/.bashrc
 ln -sfv "${DIR}/zsh/zshrc" ~/.zshrc
 ln -sfv "${DIR}/git/git-prompt.sh" ~/.git-prompt.sh
 ln -sfv "${DIR}/vim/vim/UltiSnips/javascript.snippets" ~/.config/coc/ultisnips/javascript.snippets
+ln -sfv "${DIR}/vim/vim/UltiSnips/typesciptreact.snippets" ~/.config/coc/ultisnips/typesciptreact.snippets
 ln -sfv "${DIR}/vim/vim/UltiSnips/markdown.snippets" ~/.config/coc/ultisnips/markdown.snippets
 ln -sfv "${DIR}/vim/vim/UltiSnips/mdx.snippets" ~/.config/coc/ultisnips/mdx.snippets
 ln -sfv "${DIR}/vim/vim/UltiSnips/html.snippets" ~/.config/coc/ultisnips/html.snippets
---
+++ vim/nvimrc.lua
@@ -0,0 +1,12 @@
+
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup({
+  open_on_setup = true,
+})
